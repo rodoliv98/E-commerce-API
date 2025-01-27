60b209d2fa352daa0e20de6763e3d293c32e2a8a
rodoliv98 <rdgoliv18@gmail.com>
2025-01-27 10:31:59 -0300

    Fixed some issues with historic search

--- routes/historic.js
+++ routes/historic.js
@@ -7,7 +7,7 @@ router.get('/', async (req, res) => {
     if(!req.user) return res.status(401).send('Please login');
     const ID = req.session.passport;
     try{
-        const showBuys = await Purchase.find({ "cart.userID": ID.user });
+        const showBuys = await Purchase.find({ userID: ID.user });
         if(showBuys.length === 0) return res.status(404).send('No historic');
         return res.status(200).json({ historic: showBuys });
     } catch(err){
