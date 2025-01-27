003ac563a04323234579c73a4907b9a6bdfcf94f
rodoliv98 <rdgoliv18@gmail.com>
2025-01-27 10:58:13 -0300

    Added controller folder and products controller functions

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -1,72 +1,21 @@
 import express from 'express'
-import { checkSchema, matchedData } from 'express-validator'
-import { parseQuantity, getCartTotal, reduceQuantityInDatabase, getDate } from '../utils/utilFunctions.js'
+import { checkSchema } from 'express-validator'
 import { createProduct } from '../bodySchemas/createProductSchema.js'
-import { cartSchema } from '../bodySchemas/cartSchema.js'
-import { paymentSchema } from '../bodySchemas/paymentSchema.js'
 import { updateProduct } from '../bodySchemas/updateProducts.js'
-import { Product } from '../mongooseSchemas/mongooseCreateProduct.js'
-import { Purchase } from '../mongooseSchemas/mongooseCreatePurchase.js'
+import { showProducts, showProductsById, createProductInDb, patchProductInDb, deleteProductInDb } from '../controllers/productController.js'
 import bodyValidator from '../Middlewares/bodyValidator.js'
 import idCheck from '../Middlewares/idCheck.js'
 
 const router = express.Router();
 
-router.get('/', async (req, res) => {
-    try{
-        const findItens = await Product.find();
-        if(!findItens) return res.status(404).send('Couldnt find any product');
-        return res.status(200).json({ products: findItens });
-    } catch(err){
-        console.error(err);
-        return res.status(500).json({ msg: 'Internal server error', details: err.message });
-    }
-})
+router.get('/', showProducts)
 
-router.get('/:id', idCheck, async (req, res) => {
-    try{
-        const findItens = await Product.findOne({ _id: req.params.id });
-        if(!findItens) return res.status(404).send('Product not found');
-        return res.status(200).json({ product: findItens });
-    } catch(err){
-        console.error(err);
-        return res.status(500).json({ msg: 'Internal server error', details: err.message });
-    }
-})
+router.get('/:id', idCheck, showProductsById)
 
-router.post('/', checkSchema(createProduct), bodyValidator, async (req, res) => {
-    const data = matchedData(req);
-    const newProduct = new Product(data);
-    try{
-        await newProduct.save();
-        return res.status(201).json({ msg: 'Product added to data base', product: newProduct });
-    } catch(err){
-        console.error(err);
-        return res.status(500).json({ msg: 'Internal server error', details: err.message });
-    }
-})
+router.post('/', checkSchema(createProduct), bodyValidator, createProductInDb)
 
-router.patch('/:id', checkSchema(updateProduct), bodyValidator, idCheck, async (req, res) => {
-    const data = matchedData(req);
-    try{
-        const updatedItem = await Product.findByIdAndUpdate(req.params.id, data, { new: true });
-        if(!updatedItem) return res.status(404).send('Product not found');
-        return res.status(200).json({ product: updatedItem });
-    } catch(err){
-        console.error(err);
-        return res.status(500).json({ msg: 'Internal server error', details: err.message });
-    }
-})
+router.patch('/:id', checkSchema(updateProduct), bodyValidator, idCheck, patchProductInDb)
 
-router.delete('/:id', idCheck, async (req, res) => {
-    try{
-        const deletedItem = await Product.findByIdAndDelete({ _id: req.params.id });
-        if(!deletedItem) return res.status(404).send('Product not found');
-        return res.status(200).json({ msg: 'Product deleted from data base', product: deletedItem });
-    } catch(err){
-        console.error(err);
-        return res.status(500).json({ msg: 'Internal server error', details: err.message });
-    }
-})
+router.delete('/:id', idCheck, deleteProductInDb)
 
 export default router
