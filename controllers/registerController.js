05f8fb5c6dde67f1daa39a5626fe8f4ca10f4bb2
rodoliv98 <rdgoliv18@gmail.com>
2025-01-30 12:15:25 -0300

    Added logic to check if the email already exists when creating an account

--- controllers/registerController.js
+++ controllers/registerController.js
@@ -3,8 +3,10 @@ import { User } from '../mongooseSchemas/mongooseCreateUser.js'
 import { matchedData } from "express-validator"
 
 export const createAccount = async (req, res) => {
+    const data = matchedData(req);
     try{
-        const data = matchedData(req);
+        const emailExist = await User.findOne({ email: data.email });
+        if(emailExist) return res.status(400).send('This email is already being used');
         const hashedPassword = await bcrypt.hash(data.password, 10);
         const newUser = new User({ ...data, password: hashedPassword });
         await newUser.save();
