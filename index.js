1eb65aeac276009ced38ccfb14c7b9ea52bee54f
rodoliv98 <rdgoliv18@gmail.com>
2025-03-18 15:10:01 -0300

    fixing cors

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ import 'dotenv/config'
 const app = express();
 
 const corsOptions = {
-    origin: 'https://ecommerce-front-livid.vercel.app/',
+    origin: 'https://ecommerce-front-livid.vercel.app',
     credentials: true,
     optionsSuccessStatus: 200
 };
