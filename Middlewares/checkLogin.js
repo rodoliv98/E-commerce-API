f029535d452c74f7fe26a84e400556b9b2e5834a
rodoliv98 <rdgoliv18@gmail.com>
2025-03-20 14:07:09 -0300

    Fixed login check

--- Middlewares/checkLogin.js
+++ Middlewares/checkLogin.js
@@ -1,5 +1,17 @@
-function checkLogin(req, res, next){
-    if(!req.user) return res.status(401).send('Please login')
+import { User } from "../mongooseSchemas/mongooseCreateUser.js";
+
+async function checkLogin(req, res, next){
+    const user = req.session.passport.user;
+    if(!user) return res.status(401).send('Unauthorized');
+
+    try{
+        const findUser = await User.findById(user);
+        if(!findUser) return res.status(404).send('User not found');
+        next();
+    } catch(err){
+        console.error(err.message);
+        return res.status(500).json({ msg: 'Internal server error' });
+    }
     next()
 }
 
